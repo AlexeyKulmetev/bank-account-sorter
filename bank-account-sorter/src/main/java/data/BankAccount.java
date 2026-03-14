2c26582d5d52fe53cda7e1dd1d77c84855a7625e
Marat <marat.morozov02@icloud.com>
2026-03-14 19:04:43 +0400

    "Доработан класс BankAccount"

--- bank-account-sorter/src/main/java/data/BankAccount.java
+++ bank-account-sorter/src/main/java/data/BankAccount.java
@@ -5,8 +5,56 @@ public class BankAccount {
     private final String accountHolder;
     private final double balance;
 
-    public String getAccountNumber() { return accountNumber; }
-    public String getAccountHolder() { return accountHolder; }
-    public double getBalance() { return balance; }
 
+    private BankAccount(Builder builder) {
+        this.accountNumber = builder.accountNumber;
+        this.accountHolder = builder.accountHolder;
+        this.balance = builder.balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "number='" + accountNumber + '\'' +
+                ", holder='" + accountHolder + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public static class Builder {
+        private String accountNumber;
+        private String accountHolder;
+        private double balance;
+
+        public Builder setAccountNumber(String accountNumber) {
+            this.accountNumber = accountNumber;
+            return this;
+        }
+
+        public Builder setAccountHolder(String accountHolder) {
+            this.accountHolder = accountHolder;
+            return this;
+        }
+
+        public Builder setBalance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public BankAccount build() {
+            return new BankAccount(this);
+        }
+    }
 }
